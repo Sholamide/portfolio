3f1714a00ab9ba973ac4d0e44e4b77361d71004c
Olamide <soluade101@gmail.com>
2022-05-25 02:29:15 +0800

    styled-components starter

---
+++ src/components/Themes.js
@@ -0,0 +1,18 @@
+const lightTheme = {
+  body: "#FFF",
+  text: "#363537",
+  togggleBorder: "#FFF",
+  background: "#363537",
+};
+
+const darkTheme = {
+  body: "#363537",
+  text: "#FAFAFA",
+  togggleBorder: "#6B8096",
+  background: "#999",
+};
+
+export const Themes = {
+  light: lightTheme,
+  dark: darkTheme,
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import { FormspreeProvider } from "@formspree/react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ContactSection from "../components/Contact";
 import FooterSection from "../components/Footer";
 import HeroSection from "../components/Hero";
@@ -7,24 +7,46 @@ import NavBar from "../components/NavBar";
 import ProjectsSection from "../components/Projects";
 import MobileNav from "../components/MobileNav";
 import SkillSection from "../components/Skill";
+import { ThemeProvider } from "styled-components";
+import { Themes } from "../components/Themes";
+import { GlobalStyles } from "../components/globalStyles";
 
 const Home = () => {
   const [navShow, setNavShow] = useState(false);
+  const [theme, setTheme] = useState("light");
 
-  const onToggeNav = () => {
+  const onToggleNav = () => {
     setNavShow(!navShow);
   };
+
+  // const toggleTheme = () => {
+  //   theme === "light" ? setTheme("dark") : setTheme("light");
+  // };
+
+  useEffect(() => {
+    if (localStorage.getItem("theme")) {
+      if (localStorage.getItem("theme") === "light") {
+        setTheme("light");
+      } else if (localStorage.getItem("theme") === "dark") {
+        setTheme("dark");
+      }
+    }
+  }, []);
+
   return (
     <>
-      <FormspreeProvider project={"1625156458033184489"}>
-        <MobileNav navShow={navShow} toggle={onToggeNav} />
-        <NavBar toggle={onToggeNav} />
-        <HeroSection />
-        <ProjectsSection />
-        <SkillSection />
-        <ContactSection />
-        <FooterSection />
-      </FormspreeProvider>
+      <ThemeProvider theme={Themes[theme]}>
+        <FormspreeProvider project={"1625156458033184489"}>
+          <MobileNav navShow={navShow} toggle={onToggleNav} />
+          <NavBar toggle={onToggleNav} setTheme={setTheme} />
+          <HeroSection />
+          <ProjectsSection />
+          <SkillSection />
+          <ContactSection />
+          <FooterSection />
+        </FormspreeProvider>
+        <GlobalStyles />
+      </ThemeProvider>
     </>
   );
 };
